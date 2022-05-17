c9e504003b78b6dcaf0348783d41bd74232db077
unnaschp-uwec <unnascpt8511@uwec.edu>
2022-05-17 16:26:37 -0500

    Final Push
    EldonCraft LLC

--- EldonCraft/scripts.js
+++
@@ -1,6 +0,0 @@
-function toggleNav() {
-    let elements = document.getElementsByClassName("toggle-nav");
-    for(let i = 0; i < elements.length; i++){
-    elements[i].classList.toggle('show');
-    }
-}
